b7eada210009a4acb3e6e41ccdf2437df18c3a61
Jay Gurav <mrithyunjay2799@gmail.com>
2021-01-20 19:58:02 +0530

    Firestore, Chakra UI, Absolute Imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
-import { AuthProvider } from "../lib/auth"
+import { AuthProvider } from "@/lib/auth"
+import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react"
+import { Global, css } from '@emotion/react';
+
+import myTheme from "@/styles/theme"
+
+const theme = extendTheme(myTheme);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Global
+          styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+        />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,23 @@
 import Head from 'next/head'
-import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
+import { useAuth } from '@/lib/auth'
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main >
+        <h1>
           Feedback koa🐨
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code>pages/index.js</code>
         </p>
         {auth?.user ?
           (<button onClick={() => auth.signout()}>Sign out</button>) :
@@ -26,17 +25,6 @@ export default function Home() {
         <div>{auth?.user?.email}</div>
 
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
